50010d027dabe42af8aca8ebb962b74af54bb189
nyj001012 <nyj001012@gmail.com>
2022-11-09 14:05:24 +0900

    REFACT: 불필요한 생성자 삭제

--- backend/src/books/entity/likes.entity.ts
+++ backend/src/books/entity/likes.entity.ts
@@ -13,11 +13,4 @@ export class Likes {
 
   @Column()
   isDeleted!: boolean;
-
-  constructor(id: number, bookInfoId: number, userId: number, isDeleted: boolean) {
-    this.id = id;
-    this.bookInfoId = bookInfoId;
-    this.userId = userId;
-    this.isDeleted = isDeleted;
-  }
 }
